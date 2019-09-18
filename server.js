04406bb7e82f2d4a603731fafd5de2216a743dec
METRI Laxmikant <laxmikant.metri@rntbci.com>
2019-09-18 11:46:39 +0530

    new changes

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
+// PathLocationStrategy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
